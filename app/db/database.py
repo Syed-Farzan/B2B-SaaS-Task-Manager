c91c143a8adff98a8536836ae237932e4cb786ab
Syed Arhan <arhannaqshbandi@gmail.com>
2026-06-24 08:36:26 +0530

    refactor: decouple database credentials using pydantic settings

--- app/db/database.py
+++ app/db/database.py
@@ -1,9 +1,8 @@
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from core.config import settings
 
-URL = "postgresql+asyncpg://postgres:12345@db:5432/saas_task_manager"
-
-engine = create_async_engine(url=URL)
+engine = create_async_engine(url=settings.DATABASE_URL)
 
 session_local = async_sessionmaker(
     bind=engine, autocommit=False, autoflush=False, expire_on_commit=False
